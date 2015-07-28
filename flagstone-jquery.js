7448a8dd3137eaac39a68b024c42b7acaf98cfbc
Adam Carson <adamcarson.io@gmail.com>
2015-07-28 11:05:21 -0600

    Update flagstone-jquery.js

--- flagstone-jquery.js
+++ flagstone-jquery.js
@@ -1,17 +1,26 @@
 /*
- Copyright 2014-2015 by CLEARWAVE DESIGNS, LLC.
+Copyright 2014-2015 CLEARWAVE DESIGNS, LLC
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
  
- Licensed under the Apache License, Version 2.0
- http://www.apache.org/licenses/LICENSE-2.0
+Author:        Adam Carson
+Website:       http://clearwavedesigns.com
+Dependencies:  jquery.js (1.8.3+)
  
- Author:        Adam Carson
- Website:       http://clearwavedesigns.com
- Dependencies:  jquery.js (1.8.3+)
- 
- Name(s):       FlagstoneJS (flagstone.js)
- Version:       1.3.20150723
- Slogan:        "Responsive tiling."
- Description:   Responsively tiles HTML elements left to right, top to bottom, and where there's the most room.
+Name(s):       FlagstoneJS (flagstone.js)
+Version:       1.3.20150723
+Slogan:        "Responsive tiling."
+Description:   Responsively tiles HTML elements left or right, top to bottom, and where there's the most room.
  */
 
 var FLAGSTONE = function(settings) {
