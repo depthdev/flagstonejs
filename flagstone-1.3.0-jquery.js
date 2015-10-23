1cb1b3bc2573030c7933a64db2258532c9ff4c24
Adam Carson <adamcarson.io@gmail.com>
2015-10-22 20:08:06 -0600

    Update and rename flagstone-1.3.20150803-jquery.js to flagstone-1.3.0-jquery.js

--- flagstone-1.3.0-jquery.js
+++ flagstone-1.3.0-jquery.js
@@ -1,27 +1,8 @@
 /*
-Copyright 2014-2015 CLEARWAVE DESIGNS, LLC
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
- 
-Author:        Adam Carson
-Website:       http://clearwavedesigns.com
-Dependencies:  jquery.js (1.8.3+)
- 
-Name(s):       FlagstoneJS (flagstone.js)
-Version:       1.3.20150803
-Slogan:        "Responsive tiling."
-Description:   Responsively tiles HTML elements left or right, top to bottom, and where there's the most room.
- */
+ FlagstoneJS v1.3.0-jquery
+ (c) 2015 Clearwave Designs, LLC. http://clearwavedesigns.com
+ License: Apache 2.0
+*/
 
 function FLAGSTONE(settings) {
 
